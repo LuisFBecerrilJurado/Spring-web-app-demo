20a41b5905ec7e58a75d261b83aaddfaab786acf
Fran <l.francisco.bj@gmail.com>
2023-12-05 14:22:37 -0600

    [UPDATE] Añadiendo método para enlazar al 'index.html'

--- spring-web-application/src/main/java/com/ibm/academy/microservice/springwebapplication/Controller/ModelAndViewController.java
+++ spring-web-application/src/main/java/com/ibm/academy/microservice/springwebapplication/Controller/ModelAndViewController.java
@@ -1,4 +1,15 @@
 package com.ibm.academy.microservice.springwebapplication.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
 public class ModelAndViewController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String getIndex(){
+        return "index.html";
+    }
 }
