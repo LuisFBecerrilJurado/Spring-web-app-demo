53e5129a222fc824434ab9845273855df45f18eb
Fran <l.francisco.bj@gmail.com>
2023-12-04 19:53:33 -0600

    [INIT] Inicaindo Repositorio para paractica de Spring-Web

---
+++ spring-web-application/src/main/java/com/ibm/academy/microservice/springwebapplication/SpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.academy.microservice.springwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplication.class, args);
+	}
+
+}
---
+++ spring-web-application/src/test/java/com/ibm/academy/microservice/springwebapplication/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.academy.microservice.springwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
