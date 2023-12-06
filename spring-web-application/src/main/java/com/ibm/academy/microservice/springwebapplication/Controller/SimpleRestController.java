2ae2c2062a7c6ee096ef2f23c7e9d1a8403a9105
Fran <l.francisco.bj@gmail.com>
2023-12-05 19:51:13 -0600

    [UPDATE] Añadiendo metodos para publicacion de número random, y posteo de un map de Strings

--- spring-web-application/src/main/java/com/ibm/academy/microservice/springwebapplication/Controller/SimpleRestController.java
+++ spring-web-application/src/main/java/com/ibm/academy/microservice/springwebapplication/Controller/SimpleRestController.java
@@ -1,4 +1,33 @@
 package com.ibm.academy.microservice.springwebapplication.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/v1/rest")
 public class SimpleRestController {
+
+    @RequestMapping(value = "/random", method = RequestMethod.GET)
+    public ResponseEntity<Integer> getValueRandom(){
+        Random r = new Random();
+        return new ResponseEntity<>(r.nextInt(), HttpStatus.OK);
+    }
+
+    @PostMapping("/method")
+    public ResponseEntity<Map<String,String>> changeMethodType(){
+        Map<String, String> mapResponse = new HashMap<>();
+        mapResponse.put("Campo1","Valor1");
+        mapResponse.put("Campo2","Valor2");
+        mapResponse.put("Campo3","Valor3");
+        mapResponse.put("Campo4","Valor4");
+        mapResponse.put("Campo5","Valor5");
+
+        return new ResponseEntity<>(mapResponse,HttpStatus.OK);
+
+    }
 }
