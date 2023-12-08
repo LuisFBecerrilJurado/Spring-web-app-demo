1dc5f03c852ed9bc89321da36f3eec7b4952f927
Fran <l.francisco.bj@gmail.com>
2023-12-07 21:46:03 -0600

    [CREATE] Añadiendo carpeta con el modelo del item

--- spring-web-application/src/main/java/com/ibm/academy/microservice/springwebapplication/model/item.java
+++ spring-web-application/src/main/java/com/ibm/academy/microservice/springwebapplication/model/item.java
@@ -1,5 +1,50 @@
 package com.ibm.academy.microservice.springwebapplication.model;
 
 public class item {
+    private Integer itemId;
+    private String itemName;
+    private String itemDescription;
+    private String itemNumber;
 
+    public item() {
+    }
+
+    public item(Integer itemId, String itemName, String itemDescription, String itemNumber) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemNumber = itemNumber;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getItemNumber() {
+        return itemNumber;
+    }
+
+    public void setItemNumber(String itemNumber) {
+        this.itemNumber = itemNumber;
+    }
 }
