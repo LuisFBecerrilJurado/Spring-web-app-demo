03916ec50fcac2ede47d531a7b1239874a3495e1
Fran <l.francisco.bj@gmail.com>
2023-12-07 21:33:15 -0600

    [CREATE] Añadiendo controlador para items

---
+++ spring-web-application/src/main/java/com/ibm/academy/microservice/springwebapplication/Controller/ItemController.java
@@ -0,0 +1,10 @@
+package com.ibm.academy.microservice.springwebapplication.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/v1/item")
+public class ItemController {
+
+}
